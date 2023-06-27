e0d2016d8f6b0f6e44c48ccf8ed6a8d3e9a22b41
dzzhar <zharifahdzikra@gmail.com>
2023-06-27 13:09:16 +0700

    Praktikum 1 PHP

--- praktikum01/array_buah.php
+++ praktikum01/array_buah.php
@@ -5,10 +5,13 @@ $arrayBuah = ["Jeruk", "Mangga", "Pisang", "Apel"];
 
 print_r($arrayBuah);
 echo "<br>";
+
 var_dump($arrayBuah);
 echo "<br>";
-foreach($arrayBuah as $buah){
-    echo $buah. "<br>";
+
+foreach ($arrayBuah as $buah) {
+    echo $buah . "<br>";
 }
+
 echo "<br>";
 echo $arrayBuah[0];
