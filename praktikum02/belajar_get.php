8c7257bf0497f8cd25929f4d951af1026148fde6
dzzhar <zharifahdzikra@gmail.com>
2023-06-27 13:10:13 +0700

    Praktikum 2 PHP

--- praktikum02/belajar_get.php
+++ praktikum02/belajar_get.php
@@ -6,8 +6,9 @@ $nomor_telepon = $_GET['nomor_telepon'];
 $program_studi = $_GET['prodi'];
 
 echo "<h1>Hasil Form Pendaftaran Mahasiswa</h1><br>";
-echo "Nama : " .$nama_mahasiswa . "<br>";
-echo "Jenis Kelamin : " .$jenis_kelamin . "<br>";
-echo "Mata Kuliah : " .$mata_kuliah . "<br>";
-echo "Nomor Telepon : " .$nomor_telepon . "<br>";
-echo "Program Studi : " .$program_studi . "<br>";
+
+echo "Nama : " . $nama_mahasiswa . "<br>";
+echo "Jenis Kelamin : " . $jenis_kelamin . "<br>";
+echo "Mata Kuliah : " . $mata_kuliah . "<br>";
+echo "Nomor Telepon : " . $nomor_telepon . "<br>";
+echo "Program Studi : " . $program_studi . "<br>";
